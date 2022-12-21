9986f990a4d0ef0f542ddae83981b48f03e24ceb
Woragarn Pimpa <aod03112544@gmail.com>
2022-12-21 07:35:59 +0700

    collectapi|new.ndm|

---
+++ api/src/handlers/getSaleAccount.js
@@ -0,0 +1,56 @@
+const Request = require("express").Request;
+const Response = require("express").Response;
+const db = require("../databased/db");
+
+const putAccountData = async (req, res) => {
+    let pdName = req.body.pd_name;
+    let pdType = req.body.pd_type;
+    let pdBrand = req.body.pd_brand;
+    let pdPrice = req.body.pd_price;
+
+    if (pdName == "" || pdType == "" || pdBrand == "" || pdPrice == "")
+    {
+        let response = {
+            code: 401,
+            message: "Invalid Input is empty."
+        }
+        res.json(response);
+        res.end();
+        return;
+    } else {
+        let sql = "INSERT INTO collect_sales(pd_name, pd_type, pd_brand, pd_price, pd_time) VALUES(?, ?, ?, ?, ?)";
+        let result = db.query(sql, [pdName, pdType, pdBrand, pdPrice, new Date()]);
+        console.log(result);
+        try {
+            if (result[0] && result.affectedRows > 0)
+            {
+                let response = {
+                    code: 200,
+                    message: "Success!",
+                    result: result[0],
+                }
+                res.json(response);
+                res.end();
+                return;
+            }
+            else 
+            {
+                let response = {
+                    code: 402,
+                    message: "Failed!",
+                }
+                res.json(response);
+                res.end();
+                return;
+            }
+            return true;
+        } catch (err) {
+            console.log(err.message);
+            return false;
+        }
+    }
+}
+
+module.exports = {
+    putAccountData
+};
--- web/includes/constant/index.php
+++ web/includes/constant/index.php
@@ -2,5 +2,5 @@
 
 define("CODE_SUCCESS", 200);
 
-difine("BASE_URL", "https://localhost/pos_system/pos_system/web");
-difine("API_URL", "https://localhost/pos_system/pos_system/api/src");
+define("BASE_URL", "https://localhost/project_pos/pos-system/web");
+define("API_URL", "https://localhost/project_pos/pos-system/api/src");
