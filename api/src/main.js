6b681bc5acb37795c67c751dc73d919d0fa70d58
Cion3p <cion3p@gmail.com>
2022-12-21 14:38:15 +0700

    success web to api

--- api/src/main.js
+++ api/src/main.js
@@ -6,7 +6,8 @@ const PORT = 3000;
 app.use(express.urlencoded({extended:false}))
 
 app.post("/api/sale",salehandlers.checksalehandlers);  //ยอดขายรายเดือนของพีช
-
+app.post("/api/lsale",salehandlers.lchecksalehandlers);  //ยอดขายแบบละเอียด
+app.post("/api/stat",salehandlers.statsalehandlers); //สถิติขายพีช
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
--- web/admin/stat.php
+++ web/admin/stat.php
@@ -1,37 +1,108 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 </head>
-<body>
-<canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+<?php
 
-<script>
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-var yValues = [55, 49, 44, 24, 15];
-var barColors = ["red", "green","blue","orange","brown"];
-
-new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: xValues,
-    datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    title: {
-      display: true,
-      text: "World Wine Production 2018"
-    }
+$stat = "";
+if (count($_POST) > 0) {
+  if (isset($_POST['tdata'])) {
+    $tdata = $_POST['tdata'];
   }
-});
-</script>
+
+  $url = "http://localhost:3000/api/stat";
+  $data = [
+    'tdata' => $tdata,
+  ];
+  $response = curl_post($url, $data);
+  $stat = $response['data'];
+  // echo "<pre>";
+  // print_r($stat);
+  // echo "</pre>";
+  // die();
+  $x = [];
+  foreach ($stat as $xvalue){
+    $x[] = $xvalue['TYPE'];
+}
+  $y = [];
+  foreach ($stat as $yvalue){
+    $y[] = $yvalue['total'];
+}
+
+
+
+}
+function curl_post($url, $data)
+{
+
+  $cURLConnection = curl_init($url);
+  curl_setopt($cURLConnection, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, json_encode($data));
+  curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+  $apiResponse = curl_exec($cURLConnection);
+  curl_close($cURLConnection);
+
+  // $apiResponse - available data from the API request
+  $jsonArrayResponse = json_decode($apiResponse, true);
+  return $jsonArrayResponse;
+}
+
+
+
+
+?>
+
+<body>
+  <?php if (count($_POST) > 0) : ?>
+
+
+    <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+    <script>
+      var xValues = JSON.parse('<?php echo json_encode($x, JSON_UNESCAPED_UNICODE) ?>');
+      var yValues = JSON.parse('<?php echo json_encode($y) ?>');
+      var barColors = "red";
+
+      new Chart("myChart", {
+        type: "bar",
+        data: {
+          labels: xValues,
+          datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+          }]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            text: "Stat"
+          }
+        }
+      });
+      console.log(xValues)
+    </script>
+  <?php endif; ?>
+  <form action="" method="post">
+    <label for="tdata">เลือกประเภทข้อมูล</label>
+    <input list="tdata" name="tdata">
+    <datalist id="tdata">
+      <option value="ประเภทสินค้า">
+      <option value="แบรนสินค้า">
+    </datalist>
+    <input type="submit">
+  </form>
+
+
 </body>
+
 </html>
