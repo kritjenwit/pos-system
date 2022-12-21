0128ed522263d33ef024855544b5043241cb5f9e
kritjenwit <jenwitkmonz@gmail.com>
2022-12-21 09:59:18 +0700

    Refactor code

--- api/main.js
+++ api/main.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const salehandlers = require("./src/handlers/sale");
 const saleAccount = require("./src/handlers/getSaleAccount");
-app = express();
+const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "http://localhost:3000",
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:false }))
+app.use(express.urlencoded({ extended: false }));
 
-app.post("/api/sale",salehandlers.checksalehandlers);  //ยอดขายรายเดือนของพีช
+app.post("/api/sale", salehandlers.checksalehandlers); //ยอดขายรายเดือนของพีช
 
-app.post('/api/putAccount', saleAccount.putAccountData); //push data sale of "Oat"
+app.post("/api/putAccount", saleAccount.putAccountData); //push data sale of "Oat"
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
